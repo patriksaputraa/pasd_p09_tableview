ab53523fb8bf760a0b5cc4f08fcc5d163d799fe6
patriksaputraa <103385269+patriksaputraa@users.noreply.github.com>
2023-05-29 12:42:19 +0700

    Delete P09_VSC_TableView directory

--- P09_VSC_TableView/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example {
-    requires transitive javafx.graphics;
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.javafx to javafx.fxml;
-    exports com.javafx;
-}
